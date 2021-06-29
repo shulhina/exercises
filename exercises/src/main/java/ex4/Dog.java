c103de00092af317a90b51f9056f2d5819f6759d
My name <me.example.com>
2021-06-29 08:16:45 -0500

    -

--- exercises/src/main/java/ex4/Dog.java
+++ exercises/src/main/java/ex4/Dog.java
@@ -5,6 +5,41 @@ public class Dog {
     private int age;
     private double weight;
 
+    public Dog() {
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Dog(int age, double weight) {
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public Dog(String name, int age, double weight) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public Dog(int age) {
+        this.age = age;
+    }
+
+    public Dog(double weight) {
+        this.weight = weight;
+    }
+
+    public void showExcitement(){
+        System.out.println(this.name + " is wagging its tail!!!");
+    }
+
     @Override
     public String toString(){
 
