23c9230213fcd624a07c97987d21e2be528e1a5a
My name <me.example.com>
2021-06-29 10:13:55 -0500

    -

--- exercises/src/main/java/Day2/ex1/InheritanceExample.java
+++ exercises/src/main/java/Day2/ex1/InheritanceExample.java
@@ -1,7 +1,35 @@
 package Day2.ex1;
 
+class Cycle{
+
+    String define_me(){
+
+        return "a vehicle with pedals.";
+
+    }
+
+}
+
+class Bike extends Cycle{
+
+    String define_me(){
+
+        return "a cycle with an engine.";
+
+    }
+
+    Bike(){
+        System.out.println("Hello I am a Bike I am "+ super.define_me());
+        String temp=define_me();
+
+        System.out.println("My ancestor is a cycle who is "+temp );
+
+    }
+
+}
+
 public class InheritanceExample {
     public static void main(String[] args) {
-
+        Bike M = new Bike();
     }
 }
