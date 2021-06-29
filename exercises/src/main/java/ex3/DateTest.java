807914629fe1a67f2f30f7f529e739331606636f
shulhina <86314892+shulhina@users.noreply.github.com>
2021-06-28 20:52:05 -0500

    Add files via upload

---
+++ exercises/src/main/java/ex3/DateTest.java
@@ -0,0 +1,8 @@
+package ex3;
+
+public class DateTest {
+    public static void main(String[] args) {
+        Date date = new Date(4,3,2010);
+        date.displayDate();
+    }
+}
---
+++ exercises/src/main/java/ex4/Dog.java
@@ -0,0 +1,14 @@
+package ex4;
+
+public class Dog {
+    private String name;
+    private int age;
+    private double weight;
+
+    @Override
+    public String toString(){
+
+        return null;
+    }
+
+}
