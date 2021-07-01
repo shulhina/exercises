8c29215e3acbbbf984d73001b552730ed382df54
My name <me.example.com>
2021-07-01 13:06:58 -0500

    - day 5 exercise 1

--- src/main/java/day5/ex1/Ex1.java
+++ src/main/java/day5/ex1/Ex1.java
@@ -4,6 +4,7 @@ public class Ex1 {
     public static void main(String[] args) {
         System.out.println(maxCharacter("aaafbbbdeeeda"));
         System.out.println(maxCharacter("eeddfssses"));
+        System.out.println(maxCharacter("eeddfssesee"));
     }
 
     public static int maxCharacter(String str) {
@@ -11,14 +12,11 @@ public class Ex1 {
         // 256 characters exist
         int[] count = new int[256];
         int len = str.length();
-
-        for (int i = 0; i < len; i++)
-            count[str.charAt(i)]++;
-
         int max = -1;
         char result = ' ';
 
         for (int i = 0; i < len; i++) {
+            count[str.charAt(i)]++;
             if (max < count[str.charAt(i)]) {
                 max = count[str.charAt(i)];
                 result = str.charAt(i);
