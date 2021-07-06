a2e9d0df4c25b3b7021788757b39c89322a368e0
My name <me.example.com>
2021-07-06 13:56:13 -0500

    exercise 2 day 7

--- src/main/java/day7/ex2/ex2.java
+++ src/main/java/day7/ex2/ex2.java
@@ -1,4 +1,22 @@
 package day7.ex2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ex2 {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        try{
+            Scanner sc=new Scanner(System.in);
+            int A=sc.nextInt();
+            int B=sc.nextInt();
+            System.out.println(A/B);
+        } catch(ArithmeticException | InputMismatchException e) {
+            if (e instanceof ArithmeticException) {
+                System.out.println("java.lang.ArithmeticException: / by zero");
+            } else if (e instanceof InputMismatchException){
+                System.out.println("java.util.InputMismatchException");
+            }
+        }
+    }
 }
