945138df72acd656acdb8a3e2ac8299944b2a229
shulhina <yelyzaveta_shulhina@hcl.com>
2021-07-06 15:05:13 -0500

    - day 7 exercise 5

---
+++ src/main/java/day7/ex5/ex5.java
@@ -0,0 +1,20 @@
+package day7.ex5;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) throws FileNotFoundException {
+        //BufferedReader file = new BufferedReader();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the total runs scored: ");
+        int runsScored = sc.nextInt();
+        System.out.println("Enter the total overs faced: ");
+        int totalOvers = sc.nextInt();
+
+
+    }
+
+}
