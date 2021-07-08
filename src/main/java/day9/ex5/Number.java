8df1a010779765d38882c1b5c070f8c0ffb88a4f
shulhina <yelyzaveta_shulhina@hcl.com>
2021-07-08 17:00:50 -0500

    - day 9 exercises

--- src/main/java/day9/ex5/Number.java
+++ src/main/java/day9/ex5/Number.java
@@ -1,30 +1,45 @@
 package day9.ex5;
 
 public class Number implements Runnable {
-    public void run(int number) {
-        System.out.print("Multiple of " + number + " are: ");
-        for (int i = 1; i <= number; i++) {
-            if (number % i == 0) {
+    int number;
+
+    Number (int number){
+        this.number = number;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + ": " + Thread.currentThread().getState());
+        System.out.println("Multiple of " + this.number + " are: ");
+        for (int i = 1; i <= this.number; i++) {
+            if (this.number % i == 0) {
                 System.out.print(i + " ");
             }
         }
         System.out.println();
     }
 
-    @Override
-    public void run() {
-
-    }
 
     public static void main(String[] args) {
-        Number n1 = new Number();
-        n1.run(2);
+        Number n1 = new Number(2);
+        Thread t1 = new Thread(n1, "t1");
 
-        Number n2 = new Number();
-        n2.run(5);
+        Number n2 = new Number(5);
+        Thread t2 = new Thread(n2, "t2");
 
-        Number n3 = new Number();
-        n3.run(8);
+        Number n3 = new Number(8);
+        Thread t3 = new Thread(n3, "t3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try {
+            t1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
