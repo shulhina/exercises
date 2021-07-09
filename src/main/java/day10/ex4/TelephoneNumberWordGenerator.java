6741976b022c95255f5d2164a041b495dffc15bb
shulhina <yelyzaveta_shulhina@hcl.com>
2021-07-09 13:58:21 -0500

    - day 10 exercise 4

--- src/main/java/day10/ex4/TelephoneNumberWordGenerator.java
+++ src/main/java/day10/ex4/TelephoneNumberWordGenerator.java
@@ -7,26 +7,52 @@ public class TelephoneNumberWordGenerator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number to encode!");
-        int number = sc.nextInt();
-
+        String number = sc.next();
+        System.out.println(numberWordGenerator(number));
     }
 
-    public String numberWordGenerator(String phoneNumber){
-        StringBuilder result = null;
-        for (String number : phoneNumber.split("")){
+    public static String numberWordGenerator(String phoneNumber) {
+        StringBuilder result = new StringBuilder();
+        for (String number : phoneNumber.split("")) {
             Random random = new Random();
             int index = random.nextInt(3);
-            switch(number){
+            String casePhone;
+            switch (number) {
                 case "1":
                     break;
                 case "2":
-                    String case1 = "ABC";
-                    result.append(case1.charAt(index));
+                    casePhone = "ABC";
+                    result.append(casePhone.charAt(index));
                     break;
                 case "3":
-                    String case2 = "DEF";
-                    result.append(case2.charAt(index));
+                    casePhone = "DEF";
+                    result.append(casePhone.charAt(index));
                     break;
+                case "4":
+                    casePhone = "GHI";
+                    result.append(casePhone.charAt(index));
+                    break;
+                case "5":
+                    casePhone = "JKL";
+                    result.append(casePhone.charAt(index));
+                    break;
+                case "6":
+                    casePhone = "MNO";
+                    result.append(casePhone.charAt(index));
+                    break;
+                case "7":
+                    casePhone = "PRS";
+                    result.append(casePhone.charAt(index));
+                    break;
+                case "8":
+                    casePhone = "TUV";
+                    result.append(casePhone.charAt(index));
+                    break;
+                case "9":
+                    casePhone = "WXYZ";
+                    result.append(casePhone.charAt(index));
+                    break;
+
 
             }
         }
