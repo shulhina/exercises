7a4627db2c5d3cf60ea6ea25ef2b5990088a348a
shulhina <yelyzaveta_shulhina@hcl.com>
2021-07-12 18:39:01 -0500

    formatted

--- src/main/java/day9/exercise4/DemoThread1.java
+++ src/main/java/day9/exercise4/DemoThread1.java
@@ -1,25 +1,23 @@
 package day9.exercise4;
 
-public class DemoThread1 extends Thread{
+public class DemoThread1 extends Thread {
 
     public static void main(String[] args) {
-        DemoThread1 t1 = new DemoThread1();
-        DemoThread1 t2 = new DemoThread1();
-        DemoThread1 t3 = new DemoThread1();
+        DemoThread1 rc1 = new DemoThread1();
+        Thread t1 = new Thread(rc1);
+        t1.start();
+        DemoThread1 rc2 = new DemoThread1();
+        Thread t2 = new Thread(rc2);
+        t2.start();
+        DemoThread1 rc3 = new DemoThread1();
+        Thread t3 = new Thread(rc3);
+        t3.start();
 
-    }
-
-    public DemoThread1() {
-        // working just with run();
-
-        DemoThread1 rc = new DemoThread1();
-        Thread t = new Thread(rc);
-        t.start();
     }
 
     public void run() {
         System.out.println("running child Thread in loop : ");
-        for (int i=1; i <= 10; i++){
+        for (int i = 1; i <= 10; i++) {
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
